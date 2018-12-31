def0fc8f948f99ac01171191a34847e894be42d6
ipsita Pattanaik <ipsitapattanaik.cse@gmail.com>
2018-12-31 04:49:47 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
+def Operations
+end 
 def unsafe?(speed)
+  if speed > 60
+    puts true
+    elsif speed < 40
+    puts true
+    elsif speed > 40 && speed < 60
+    puts false
 
 end
-
-
 
 def not_safe?(speed)
 	
 end
-	
-
+	unsafe?(35)
+unsafe?(79)
+unsafe?(50)
 
