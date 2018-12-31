1d3065c63ba45647f821224b92789421f92363d0
ipsita Pattanaik <ipsitapattanaik.cse@gmail.com>
2018-12-31 05:27:37 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -14,8 +14,11 @@ unsafe?(79)
 unsafe?(50)
 
 def not_safe?(speed)
+  seed = 79
 speed > 60 ? true : false
+speed = 35
 speed < 40 ? true : false
+speed = 50
 speed > 40 && speed < 60 ? true : false
 end
 not_safe?(35)
